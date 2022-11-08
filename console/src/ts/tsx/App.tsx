4f73f155be71378a22d5b859b9928222e44cdfcf
red2fred2 <red2fred2base@live.com>
2022-11-07 20:10:21 -0600

    Set colors

--- console/src/ts/tsx/App.tsx
+++ console/src/ts/tsx/App.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react'
-import Button from '@mui/material/Button'
+import {Button, ThemeProvider} from '@mui/material'
+import{createTheme} from '@mui/material/styles'
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#5c6bc0',
+		},
+		secondary: {
+			main: '#29b6f6',
+		},
+	},
+})
 
 export class App extends React.Component {
 	render() {
 		return (
-			<div>
+			<ThemeProvider theme={theme}>
 				<Button variant="contained">Hello World</Button>
-			</div>
+			</ThemeProvider>
 		)
 	}
 }
