79228654a356f29fb3ee483d2a2e8416398bff64
red2fred2 <red2fred2base@live.com>
2022-11-09 00:55:10 -0600

    Speed up browserify step Make imports more specific to speed up browserify's dependency search.

--- console/src/ts/tsx/Placeholder.tsx
+++ console/src/ts/tsx/Placeholder.tsx
@@ -1,12 +1,12 @@
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import { Card } from '@mui/material'
+import AddCircle from '@mui/icons-material/AddCircle';
+import Card from '@mui/material/Card'
 import * as React from 'react'
 
 export class Placeholder extends React.Component {
 	render() {
 		return (
 			<Card className="widget">
-				<AddCircleIcon className="centered" color="primary" sx={{ fontSize: 100 }} />
+				<AddCircle className="centered" color="primary" sx={{ fontSize: 100 }} />
 			</Card>
 		)
 	}
