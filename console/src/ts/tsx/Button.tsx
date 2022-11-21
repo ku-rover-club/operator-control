02cefdd3059e0cb99c5c7b62ffddab563e60ba57
red2fred2 <red2fred2base@live.com>
2022-11-20 20:25:11 -0600

    Add button component This component published a message string to a topic whenever pressed.
    [#255] Allow creating buttons that create a ROS topic and publish messages to it
    
    https://project.red2fred2.com/work_packages/255

---
+++ console/src/ts/tsx/Button.tsx
@@ -0,0 +1,32 @@
+import Card from '@mui/material/Card'
+import * as React from 'react'
+import MaterialButton from '@mui/material/Button'
+import { Topic } from 'roslib'
+
+export interface ButtonProps {
+	message: string,
+	name: string,
+	topic: Topic,
+}
+
+/**
+ * Button
+ * Creates a button object with a name and a topic it publishes values to.
+ */
+export class Button extends React.Component<ButtonProps> {
+
+	click(): void {
+		this.props.topic.publish(this.props.message)
+	}
+
+	render() {
+		return (
+			<MaterialButton
+				variant="contained"
+				onClick={this.click.bind(this)}
+			>
+				{this.props.name}
+			</MaterialButton>
+		)
+	}
+}
